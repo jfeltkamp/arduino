75da585eb24635c6f03fac9468c2fd1907b879d1
Joachim <joachim@the-resc.com>
2024-04-25 19:27:53 +0200

    running 2 motor test

--- Nema_17_Stepper_Motor_With_Arduino.ino
+++ Nema_17_Stepper_Motor_With_Arduino.ino
@@ -1,22 +1,32 @@
 #include <AccelStepper.h>
 #define motorInterfaceType 1
-AccelStepper stepper(motorInterfaceType, 5, 2);  //STEP-Pin, DIR-Pin
+AccelStepper stepper1(motorInterfaceType, 2, 5);  //STEP-Pin, DIR-Pin
+AccelStepper stepper2(motorInterfaceType, 3, 6);  //STEP-Pin, DIR-Pin
 
 const int enPinX = 8;
 
 void setup() {
-  stepper.setMaxSpeed(500);  // Maximale Geschwindigkeit
-  stepper.setAcceleration(100);  // Beschleunigung Schritte pro sek.^2
+  stepper1.setMaxSpeed(800);  // Maximale Geschwindigkeit
+  stepper1.setAcceleration(500);  // Beschleunigung Schritte pro sek.^2
+  stepper2.setMaxSpeed(800);  // Maximale Geschwindigkeit
+  stepper2.setAcceleration(500);  // Beschleunigung Schritte pro sek.^2
 
   pinMode(enPinX,OUTPUT);
   digitalWrite(enPinX,LOW);
 }
 
 void loop() {
-  stepper.moveTo(1000);  // Zielposition
-  stepper.runToPosition();
+  stepper1.moveTo(2400);  // Zielposition
+  stepper1.runToPosition();
   delay(1000);
-  stepper.moveTo(0);  // Anfangsposition
-  stepper.runToPosition();
+  stepper1.moveTo(0);  // Anfangsposition
+  stepper1.runToPosition();
+  delay(1000);
+
+  stepper2.moveTo(2400);  // Zielposition
+  stepper2.runToPosition();
+  delay(1000);
+  stepper2.moveTo(0);  // Anfangsposition
+  stepper2.runToPosition();
   delay(1000);
 }
