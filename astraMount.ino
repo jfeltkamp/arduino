17f5ea964ea0ad8c5274931c5ee1dba52349b9b8
Joachim <joachim@the-resc.com>
2024-06-16 23:59:39 +0200

    Enabled display of sky direction

--- astraMount.ino
+++ astraMount.ino
@@ -25,6 +25,7 @@ const int steeringTolerance = 12;
 const int acceleration = 500;
 const int maxSpeed = 930;
 const int speedInterval = floor(maxSpeed / stepSize);
+const long stepsPerRound = 80000;
 
 // variables
 int valueF = 0;
@@ -33,10 +34,12 @@ int holdF = 1;
 int valueX = 0;
 int holdX = 1;
 float speedX = 0.0;
+bool updatedX = false;
 
 int valueY = 0;
 int holdY = 1;
 float speedY = 0.0;
+bool updatedY = false;
 
 void setup()
 {
@@ -59,28 +62,96 @@ void setup()
 
 void loop()
 {
+
+  // Azimuth axis.
   valueX = getControle(analogRead(potiX), holdX);
   if (valueX != holdX) {
     holdX = valueX;
-    // lcdOut(0, 0, 'x', holdX, 8);
-    stepper1.setSpeed(valueX * speedInterval);
+    stepper2.setSpeed(valueX * -1 * speedInterval);
+    updatedX = true;
   }
-  stepper1.runSpeed();
+  stepper2.runSpeed();
 
+  if ((valueX == 0) && updatedX) {
+    long posX = stepper2.currentPosition();
+    const String& dir = getAzimuthDirection(posX);
+    lcdOut(0, 0, 'H', dir, 12);
+    updatedX = false;
+  }
+
+  // Altitude axis.
   valueY = getControle(analogRead(potiY), holdY);
   if (valueY != holdY) {
     holdY = valueY;
-    // lcdOut(0, 1, 'y', holdY, 8);
-    stepper2.setSpeed(valueY * speedInterval);
+    stepper1.setSpeed(valueY * -1 * speedInterval);
+    updatedY = true;
   }
-  stepper2.runSpeed();
+  stepper1.runSpeed();
+
+  if ((valueY == 0) && updatedY) {
+    int posY = stepper1.currentPosition();
+    const String& dir = getAltitudeDirection(posY);
+    lcdOut(0, 1, 'V', dir, 9);
+    updatedY = false;
+  }
 
   valueF = getControle(analogRead(potiF), holdF);
   if (valueF != holdF) {
     holdF = valueF;
-    lcdOut(8, 0, 'F', holdF, 8);
+    String focus = String(holdF);
+    lcdOut(11, 1, 'F', focus, 5);
   }
+}
 
+/**
+ * Get direction from step count.
+ */
+String getAzimuthDirection(long steps) {
+  long modi = steps % stepsPerRound;
+  if (modi < 0) {
+    modi = stepsPerRound + modi;
+  }
+  float result = 360.0 * (float)modi / (float)stepsPerRound;
+
+  String resStr = String(result, 2);
+  resStr.concat('"');
+  resStr.concat(' ');
+
+  String arr[17];
+  arr[0] = "N";
+  arr[1] = "NNE";
+  arr[2] = "NE";
+  arr[3] = "ENE";
+  arr[4] = "E";
+  arr[5] = "ESE";
+  arr[6] = "SE";
+  arr[7] = "SSE";
+  arr[8] = "S";
+  arr[9] = "SSW";
+  arr[10] = "SW";
+  arr[11] = "WSW";
+  arr[12] = "W";
+  arr[13] = "WNW";
+  arr[14] = "NW";
+  arr[15] = "NNW";
+  arr[16] = "N";
+  float degNom = result + 11.25;
+  int index = (float)degNom / 22.5;
+  resStr.concat(arr[index]);
+  return resStr;
+}
+
+/**
+ * Get direction from step count.
+ */
+String getAltitudeDirection(long steps) {
+  long modi = steps % stepsPerRound;
+  char dir = 'N';
+  float result = 360.0 * (float)modi / (float)stepsPerRound;
+
+  String resStr = String(result, 2);
+  resStr.concat('"');
+  return resStr;
 }
 
 
@@ -104,7 +175,7 @@ int getControle(int analogVal, int currentLevel) {
   return absLevel;
 }
 
-void lcdOut(int x, int y, char c, int v, int space) {
+void lcdOut(int x, int y, char c, const String& v, int space) {
   lcd.setCursor(x,y);
   for (int blank = 0; blank < space; blank++) {
     lcd.print(" ");
