372e3aa4fdc4442d700c851703c22d6a531f3bdc
Joachim <joachim@the-resc.com>
2024-08-07 12:06:12 +0200

    astroMount_raspi python

---
+++ astroMount_raspi/astroMount_raspi.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env_python3
+import serial
+import time
+
+while True:
+    try:
+        ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0)
+        time.sleep(3)
+        ser.reset_input_buffer()
+        print("Serial connection OK.")
+        break
+    except serial.SerialException:
+        print("Could not connect to serial.")
+        time.sleep(1)
+
+
+def asterix_command(cmd, param, await_resp=True):
+    cmd_serial = f"{cmd}:{param}\n"
+    ser.write(cmd_serial.encode('utf-8'))
+    if await_resp:
+        fired_time = time.time()
+        while True:
+            time.sleep(0.1)
+            if ser.in_waiting > 0:
+                resp = ser.readline().decode('utf-8').rstrip()
+                if resp == "success":
+                    print(f"Command '{cmd}: {param}' was successful.")
+                    break
+                if resp == "error":
+                    raise Exception(f"Error on executing command '{cmd}: {param}'.")
+                    break
+            if time.time() - fired_time > 10:
+                raise Exception(f"Command '{cmd}: {param}' timed out.")
+
+
+commands = {
+    "m_up": 10000,
+    "m_left": 10000,
+    "m_down": 10000,
+    "m_right": 10000,
+}
+# Fires commands.
+try:
+    for command, value in commands:
+        asterix_command(command, value)
+    print("Program finished.")
+except:
+    print("Command failed.")
+finally:
+    ser.close()
+    print("Serial connection closed.")
