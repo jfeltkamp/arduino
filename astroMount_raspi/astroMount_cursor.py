b9ba62d617557c38db4b0e32c14dfdb6292d2749
Joachim <joachim@the-resc.com>
2024-10-02 21:49:43 +0200

    Cursor library

--- astroMount_raspi/astroMount_cursor.py
+++ astroMount_raspi/astroMount_cursor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env_python3
 from time import sleep
 import serial
-import keyboard
+from pynput import keyboard
 
 while True:
     try:
@@ -14,6 +14,7 @@ while True:
         print("Could not connect to serial.")
         sleep(1)
 
+class MyException(Exception): pass
 
 def asterix_command(cmd, param, await_resp=False):
     cmd_serial = f"{cmd}:{param}\n"
@@ -25,24 +26,28 @@ global releaseListening
 keepListening = True
 
 def key_press(key):
-    if key.name == "left":
+    print(f"RUN {key}")
+    if key == keyboard.Key.left:
         asterix_command("cmd_left", "400")
-    if key.name == "right":
+    if key == keyboard.Key.right:
         asterix_command("cmd_right", "400")
-    if key.name == "up":
+    if key == keyboard.Key.up:
         asterix_command("cmd_up", "400")
-    if key.name == "down":
+    if key == keyboard.Key.down:
         asterix_command("cmd_down", "400")
+    if key == keyboard.Key.esc:
+        raise MyException(key)
 
 def key_release(key):
-    print(f"stop {key.name}")
-    if key.name in ["left", "right"]:
+    print(f"stop {key}")
+    if key in [keyboard.Key.left, keyboard.Key.right]:
         asterix_command("cmd_stop", "x")
-    if key.name in ["up", "down"]:
+    if key in [keyboard.Key.up, keyboard.Key.down]:
         asterix_command("cmd_stop", "y")
 
-keyboard.on_press(key_press, True)
-keyboard.on_release(key_release, True)
-
-while keepListening:
-    sleep(1000)
+# Collect events until released
+with keyboard.Listener(on_press=key_press, on_release=key_release) as listener:
+    try:
+        listener.join()
+    except MyException as e:
+        print('{0} was pressed'.format(e.args[0]))
