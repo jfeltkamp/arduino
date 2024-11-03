a1e17e174bfe4f6ca3585023842bc7037fcf1403
Joachim <joachim@the-resc.com>
2024-11-03 18:44:45 +0100

    Added picamera2

--- obelix_raspi/static/js/socket.js
+++ obelix_raspi/static/js/socket.js
@@ -12,3 +12,17 @@ socket.on('message', function(data) {
   }
 });
 
+class TelescopeStream {
+  constructor() {
+    this.streamElement = document.getElementById('telescope-stream');
+    if (this.streamElement) {
+      this.setSource();
+    }
+  }
+
+  setSource() {
+      this.streamElement.src = 'http://' + location.hostname + ':' + '7777/stream.mjpg?t=' + Date.now();
+  }
+}
+
+new TelescopeStream()
