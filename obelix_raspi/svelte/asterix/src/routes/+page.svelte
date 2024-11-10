29eb08c9c05e7c9b80e2da27fd23e9f3505f3f87
Joachim <joachim@the-resc.com>
2024-11-10 18:06:42 +0100

    Write config updates from Arduino into store

--- obelix_raspi/svelte/asterix/src/routes/+page.svelte
+++ obelix_raspi/svelte/asterix/src/routes/+page.svelte
@@ -1,6 +1,31 @@
 <script>
+  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
   import Visuals from "../components/Visuals.svelte";
   import Controls from "../components/Controls.svelte";
+  import { arduinoSettings } from "$lib/data-store.js"
+
+  let socketIo;
+  const connectIO = (io) => {
+    socketIo = io.connect('http://192.168.178.38:5000');
+    // io.connect('http://' + location.hostname + ':' + location.port);
+
+    socketIo.on('connect', () => {
+      console.log('Websocket connected.');
+    });
+
+    socketIo.on('message', (data) => {
+      if (typeof data === 'object' && !Array.isArray(data) && data !== null) {
+        arduinoSettings.update((storeData) => { return { ...storeData, ...data }; });
+      }
+    });
+  }
+
+  onMount(() => {
+    if (browser && (typeof io === 'function')) {
+        connectIO(io);
+    }
+  })
 </script>
 
 <div class="grid-container">
